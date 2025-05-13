f6ad8ac285961949b37e723f69bb651a8dc27c86
iMikeLi <mlinz20@yahoo.com>
2025-05-13 15:27:30 +0200

    CreateShoppingListPage Erstellen und Löschen von Listen
    "Um Einkaufslisten auf ""CreateShoppingListPage"" erstellen und löschen zu können habe ich:
    ShoppingList.cs erstellt
    CreateShoppingListPage.xaml bearbeitet
    CreateShoppingListPage.xaml.cs bearbeitet"

---
+++ Models/ShoppingList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinkaufslistenApp.Models
+{
+    public class ShoppingList
+    {
+        public string? Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
--- Pages/CreateShoppingListPage.xaml.cs
+++ Pages/CreateShoppingListPage.xaml.cs
@@ -1,9 +1,42 @@
-namespace EinkaufslistenApp;
+using System;
+using System.Collections.ObjectModel; // Wichtig für ObservableCollection!
+using EinkaufslistenApp.Models;
+using Microsoft.Maui.Controls;
 
-public partial class CreateShoppingListPage : ContentPage
+namespace EinkaufslistenApp
 {
-	public CreateShoppingListPage()
-	{
-		InitializeComponent();
-	}
+    public partial class CreateShoppingListPage : ContentPage
+    {
+        // Hier speichern wir alle Einkaufslisten
+        private ObservableCollection<ShoppingList> shoppingLists = new ObservableCollection<ShoppingList>();
+
+        public CreateShoppingListPage()
+        {
+            InitializeComponent();
+
+            // Die Liste mit der Benutzeroberfläche verbinden
+            ShoppingListsView.ItemsSource = shoppingLists;
+        }
+
+        private void OnCreateShoppingListClicked(object sender, EventArgs e)
+        {
+            var newList = new ShoppingList
+            {
+                Name = "Neue Einkaufsliste",
+                CreatedAt = DateTime.Now
+            };
+
+            shoppingLists.Insert(0, newList);
+        }
+
+
+        private void OnDeleteShoppingListClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.CommandParameter is ShoppingList list)
+            {
+                shoppingLists.Remove(list);
+            }
+        }
+
+    }
 }
